f038fbc96fa14d1f2288d929c431da5d9235bf68
sergii.pavlenko <sm.pavlenko@gmail.com>
2020-07-08 18:28:53 +0200

    Python script for building Docker image

--- build-docker-image.py
+++ build-docker-image.py
@@ -7,4 +7,9 @@ def exportDockerProperties():
     os.system('eval $(minikube docker-env)')
 
 
+def buildDockerImage():
+    os.system('docker build -t smpavlenko/my-jenkins-image:2.0 .')
+
+
 exportDockerProperties()
+buildDockerImage()
