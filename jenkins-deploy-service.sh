d4087933e89e1319179cbced32dc5e3b8b88879d
sergii.pavlenko <sm.pavlenko@gmail.com>
2020-07-17 19:39:02 +0200

    Added extracting required data from k8s resources

--- jenkins-deploy-service.sh
+++ jenkins-deploy-service.sh
@@ -8,5 +8,7 @@ MINIKUBE_IP=$(minikube ip)
 JENKINS_PORT=$(kubectl get service -o json | jq -r '.items[] | select (.metadata.name=="jenkins") | .spec.ports[].nodePort')
 TARGET_PORT=$(kubectl get service -o json | jq -r '.items[] | select (.metadata.name=="kubernetes") | .spec.ports[].targetPort')
 JENKINS_POD_IP=$(kubectl get pods -o json | jq -r '.items[] | select (.metadata.labels.app=="jenkins") | .status.podIP')
+SERVER_URL="https://$MINIKUBE_IP:$TARGET_PORT"
+JENKINS_URL="http://$JENKINS_POD_IP:8080"
 JENKINS_UI_URL="http://$MINIKUBE_IP:$JENKINS_PORT"
-echo $JENKINS_UI_URL
+echo "Jenkins is reachable by $JENKINS_UI_URL"
