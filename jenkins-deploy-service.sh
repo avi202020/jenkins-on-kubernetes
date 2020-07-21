31fe08b85c3b5473919ae67a97350554d670a7d0
sergii.pavlenko <sm.pavlenko@gmail.com>
2020-07-21 21:13:16 +0200

    Updated k8s deployment script

--- jenkins-deploy-service.sh
+++ jenkins-deploy-service.sh
@@ -5,10 +5,15 @@ kubectl apply -f jenkins-rbac.yaml
 kubectl apply -f jenkins-service.yaml
 
 MINIKUBE_IP=$(minikube ip)
-JENKINS_PORT=$(kubectl get service -o json | jq -r '.items[] | select (.metadata.name=="jenkins") | .spec.ports[].nodePort')
 TARGET_PORT=$(kubectl get service -o json | jq -r '.items[] | select (.metadata.name=="kubernetes") | .spec.ports[].targetPort')
 JENKINS_POD_IP=$(kubectl get pods -o json | jq -r '.items[] | select (.metadata.labels.app=="jenkins") | .status.podIP')
 SERVER_URL="https://$MINIKUBE_IP:$TARGET_PORT"
 JENKINS_URL="http://$JENKINS_POD_IP:8080"
+
+PWD=$(pwd)
+sed -i "" "s|{JENKINS_URL}|$JENKINS_URL|g" $PWD/var/jenkins_home/config.xml
+sed -i "" "s|{SERVER_URL}|$SERVER_URL|g" $PWD/var/jenkins_home/config.xml
+
+JENKINS_PORT=$(kubectl get service -o json | jq -r '.items[] | select (.metadata.name=="jenkins") | .spec.ports[].nodePort')
 JENKINS_UI_URL="http://$MINIKUBE_IP:$JENKINS_PORT"
 echo "Jenkins is reachable by $JENKINS_UI_URL"
