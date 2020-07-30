e03f25ef4f63cb5224e017c05e7c9a7e3d1749d9
sergii.pavlenko <sm.pavlenko@gmail.com>
2020-07-30 18:06:30 +0200

    Refactored docker container building script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-REPO=smpavlenko/my-jenkins-image
-TAG=1.0
+function build() {
+    REPO=smpavlenko/my-jenkins-image
+    TAG=1.0
 
-eval $(minikube docker-env)
-docker build -t $REPO:$TAG .
+    eval $(minikube docker-env)
+    docker build -t $REPO:$TAG .
+}
+
+build
